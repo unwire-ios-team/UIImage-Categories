1496ef0f8af97e7fdf7faadaeb84dc2ad54c7c86
Lukasz Marcin Margielewski <lmm@unwire.dk>
2018-09-25 12:40:26 +0200

    Adds dummy swift file to generate -Swift.h header

---
+++ UIImage-Categories/Dummy.swift
@@ -0,0 +1,11 @@
+//
+//  Dummy.swift
+//  UIImage-Categories
+//
+//  Created by Lukasz Margielewski on 25/09/2018.
+//  Copyright © 2018 Unwire ApS. All rights reserved.
+//
+
+import Foundation
+
+class Dummy {}
